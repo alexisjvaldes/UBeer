bb071d93830ed15fd2c4bb4311513a32a23b8d47
Connor Kennard <cgkennar@email.uark.edu>
2019-03-14 16:02:38 -0500

    test stuff

--- UBeer/models.py
+++ UBeer/models.py
@@ -1,5 +1,61 @@
 from django.db import models
 
 
-class User(models.Model):
-    username = models.TextField()
+class Users(models.Model):
+    ROLE_CHOICES = (
+        ('R', 'Rider'),
+        ('E', 'Establishment')
+    )
+
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.EmailField()
+    role = models.CharField(choices=ROLE_CHOICES, max_length=1)
+
+    class Meta(object):
+        unique_together = ('username',)
+
+    def is_valid(self, _username, _password):
+        return self.username == _username and self.password == _password
+
+    def is_rider(self):
+        return self.role == 'R'
+
+    def is_establishment(self):
+        return self.role == 'E'
+
+
+class Establishments(models.Model):
+    user = models.ForeignKey(Users, on_delete=None)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+    address = models.CharField(max_length=128)
+    zipCode = models.CharField(max_length=5)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=2)
+
+    def save(self, *args, **kwargs):
+        try:
+            int(self.zipCode)
+        except ValueError:
+            raise Exception("Invalid Zip Code")
+
+        super(Establishments).save(self, *args, *kwargs)
+
+    
+class Menu(models.Model):
+    establishment = models.ForeignKey(Establishments, on_delete=None)
+    name = models.CharField(max_length=128)
+    price = models.FloatField()
+
+
+class Transactions(models.Model):
+    user = models.ForeignKey(Users, on_delete=None)
+    total = models.FloatField()
+    date = models.DateTimeField()
+    establishment = models.ForeignKey(Establishments, on_delete=None)
+
+
+class TransactionsMenu(models.Model):
+    transaction = models.ForeignKey(Transactions, on_delete=None)
+    menuID = models.ForeignKey(Menu, on_delete=None)
